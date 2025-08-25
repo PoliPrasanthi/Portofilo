fc0595a7c4bf75f69f6c3d74a22dbc031205f859
Prasanthi Poli <prasanthipoli2003@gmail.com>
2025-08-25 13:02:29 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const tableBody = document.getElementById("data-body");
 
-  let storedData = JSON.parse(localStorage.getItem("contactData")) || [];
-
-  if (storedData.length === 0) {
-    tableBody.innerHTML = `<tr><td colspan="3" class="text-center text-muted">No messages found.</td></tr>`;
-  } else {
-    storedData.forEach(entry => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-        <td>${entry.name}</td>
-        <td>${entry.email}</td>
-        <td>${entry.message}</td>
-      `;
-      tableBody.appendChild(row);
-    });
-  }
-});
